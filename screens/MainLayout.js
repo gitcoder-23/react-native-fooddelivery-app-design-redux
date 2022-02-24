b9c566c72a38ef9602360a30796ed267f2558a99
prabhat-cn <prabhat@capitalnumbers.com>
2022-02-24 18:20:34 +0530

    ⚡️

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -26,32 +26,45 @@ import {
 } from '../constants';
 import { Header } from '../components';
 
-const TabButton = ({ label, icon, onPress, isFocused }) => {
+const TabButton = ({
+  label,
+  icon,
+  onPress,
+  isFocused,
+  outerContainerStyle,
+  innerContainerStyle,
+}) => {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <Animated.View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Animated.View
-          style={{
-            flexDirection: 'row',
-            width: '30%',
-            height: 50,
+        style={[
+          {
+            flex: 1,
             alignItems: 'center',
             justifyContent: 'center',
-            borderRadius: 25,
-          }}
+          },
+          outerContainerStyle,
+        ]}
+      >
+        <Animated.View
+          style={[
+            {
+              flexDirection: 'row',
+              width: '30%',
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 25,
+            },
+            innerContainerStyle,
+          ]}
         >
           <Image
             source={icon}
             style={{
               width: 20,
               height: 20,
-              tintColor: COLORS.gray,
+              tintColor: isFocused ? COLORS.white : COLORS.gray,
             }}
           />
 
@@ -79,9 +92,127 @@ const MainLayout = ({
   selectedTab,
   setSelectedTab,
 }) => {
+  // Reanimated Shared Value
+  const homeTabFlex = useSharedValue(1);
+  const homeTabColor = useSharedValue(COLORS.white);
+  const searchTabFlex = useSharedValue(1);
+  const searchTabColor = useSharedValue(COLORS.white);
+  const cartTabFlex = useSharedValue(1);
+  const cartTabColor = useSharedValue(COLORS.white);
+  const favouriteTabFlex = useSharedValue(1);
+  const favouriteTabColor = useSharedValue(COLORS.white);
+  const notificationTabFlex = useSharedValue(1);
+  const notificationTabColor = useSharedValue(COLORS.white);
+
+  // Reanimated Animated Style
+  const homeFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: homeTabFlex.value,
+    };
+  });
+
+  const homeColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: homeTabColor.value,
+    };
+  });
+
+  const searchFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: searchTabFlex.value,
+    };
+  });
+
+  const searchColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: searchTabColor.value,
+    };
+  });
+
+  const cartFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: cartTabFlex.value,
+    };
+  });
+
+  const cartColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: cartTabColor.value,
+    };
+  });
+
+  const favouriteFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: favouriteTabFlex.value,
+    };
+  });
+
+  const favouriteColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: favouriteTabColor.value,
+    };
+  });
+
+  const notificationFlexStyle = useAnimatedStyle(() => {
+    return {
+      flex: notificationTabFlex.value,
+    };
+  });
+
+  const notificationColorStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: notificationTabColor.value,
+    };
+  });
+
   React.useEffect(() => {
     setSelectedTab(constants.screens.home);
   }, []);
+
+  React.useEffect(() => {
+    if (selectedTab == constants.screens.home) {
+      homeTabFlex.value = withTiming(4, { duration: 500 });
+      homeTabColor.value = withTiming(COLORS.primary, { duration: 500 });
+    } else {
+      homeTabFlex.value = withTiming(1, { duration: 500 });
+      homeTabColor.value = withTiming(COLORS.white, { duration: 500 });
+    }
+
+    if (selectedTab == constants.screens.search) {
+      searchTabFlex.value = withTiming(4, { duration: 500 });
+      searchTabColor.value = withTiming(COLORS.primary, { duration: 500 });
+    } else {
+      searchTabFlex.value = withTiming(1, { duration: 500 });
+      searchTabColor.value = withTiming(COLORS.white, { duration: 500 });
+    }
+
+    if (selectedTab == constants.screens.cart) {
+      cartTabFlex.value = withTiming(4, { duration: 500 });
+      cartTabColor.value = withTiming(COLORS.primary, { duration: 500 });
+    } else {
+      cartTabFlex.value = withTiming(1, { duration: 500 });
+      cartTabColor.value = withTiming(COLORS.white, { duration: 500 });
+    }
+
+    if (selectedTab == constants.screens.favourite) {
+      favouriteTabFlex.value = withTiming(4, { duration: 500 });
+      favouriteTabColor.value = withTiming(COLORS.primary, { duration: 500 });
+    } else {
+      favouriteTabFlex.value = withTiming(1, { duration: 500 });
+      favouriteTabColor.value = withTiming(COLORS.white, { duration: 500 });
+    }
+
+    if (selectedTab == constants.screens.notification) {
+      notificationTabFlex.value = withTiming(4, { duration: 500 });
+      notificationTabColor.value = withTiming(COLORS.primary, {
+        duration: 500,
+      });
+    } else {
+      notificationTabFlex.value = withTiming(1, { duration: 500 });
+      notificationTabColor.value = withTiming(COLORS.white, { duration: 500 });
+    }
+  }, [selectedTab]);
+
   return (
     <Animated.View
       style={{
@@ -174,6 +305,8 @@ const MainLayout = ({
             icon={icons.home}
             isFocused={selectedTab == constants.screens.home}
             onPress={() => setSelectedTab(constants.screens.home)}
+            outerContainerStyle={homeFlexStyle}
+            innerContainerStyle={homeColorStyle}
           />
 
           <TabButton
@@ -181,24 +314,32 @@ const MainLayout = ({
             icon={icons.search}
             isFocused={selectedTab == constants.screens.search}
             onPress={() => setSelectedTab(constants.screens.search)}
+            outerContainerStyle={searchFlexStyle}
+            innerContainerStyle={searchColorStyle}
           />
           <TabButton
             label={constants.screens.cart}
             icon={icons.cart}
             isFocused={selectedTab == constants.screens.cart}
             onPress={() => setSelectedTab(constants.screens.cart)}
+            outerContainerStyle={cartFlexStyle}
+            innerContainerStyle={cartColorStyle}
           />
           <TabButton
             label={constants.screens.favourite}
             icon={icons.favourite}
             isFocused={selectedTab == constants.screens.favourite}
             onPress={() => setSelectedTab(constants.screens.favourite)}
+            outerContainerStyle={favouriteFlexStyle}
+            innerContainerStyle={favouriteColorStyle}
           />
           <TabButton
             label={constants.screens.notification}
             icon={icons.notification}
             isFocused={selectedTab == constants.screens.notification}
             onPress={() => setSelectedTab(constants.screens.notification)}
+            outerContainerStyle={notificationFlexStyle}
+            innerContainerStyle={notificationColorStyle}
           />
         </View>
       </View>
--- stores/store.js
+++ stores/store.js
@@ -2,6 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
+// const middlewares = [thunk];
+
+// if (__DEV__) {
+//   const createDebugger = require('redux-flipper').default;
+//   middlewares.push(createDebugger());
+// }
+
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
